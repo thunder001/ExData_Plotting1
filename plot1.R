a49026dd6229c1bcd7665613ce65702f55db0a51
wanfeng yu <wanfeng@gmail.com>
2015-03-08 17:32:38 -0400

    new plot1

--- plot1.R
+++ plot1.R
@@ -1,3 +1,6 @@
 setwd("d:/zhengcl/Datascience/Exploratory/project1")
 consump <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE, colClasses=c(rep("character",2), rep("numeric", 7)))
 consump_subset <- subset(consump, Date=="1/2/2007" | Date=="2/2/2007")
+hist(consump_subset$Global_active_power, main="Global Active Power", xlab="Global Active Power(killowatts)", col="red")
+dev.copy(png, file="plot1.png")
+dev.off()
